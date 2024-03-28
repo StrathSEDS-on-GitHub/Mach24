4371d609defb28c7bbc55a002a6862fbb2a7c71a
PAAI <pablo.alvarez-icaza.2019@uni.strath.ac.uk>
2024-03-28 19:19:33 +0000

    Recovery: Lengthened the nose bay by 10cm and started a shock chord script

---
+++ Mechanical/Recovery/ChuteOpening.m
@@ -0,0 +1,18 @@
+%%Chute Opening
+
+mRocket = 6.33; %kg
+mChute = 0.2; %kg
+
+%chute data
+CD = 1.2;
+diameter = 0.609;
+A = pi()*(diameter/2)^2;
+
+%Deployment Velocity
+vApogee = 100; %m/s
+
+%Materials
+ENylon = 2; %Gpa
+EKevlar = 70; %Gpa
+
+%Max extension Nylon 4.5% and Kevlar 3%
